65cfc97a8a7ea6c4199fcc085bc360ab80397ae7
Chase Hardin <chase.hardin@Chases-MacBook-Pro-2.local>
2017-06-07 18:00:12 -0500

    Add failing test - should return blank when zero.

---
+++ C-Sharp-Roman-Numeral/RomanNumeral.Business.Tests/Converters/ConvertNumberToRomanNumeralCommandExecuteTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanNumeral.Business.Converters;
+
+namespace RomanNumeral.Business.Tests.Converters
+{
+    [TestClass]
+    public class ConvertNumberToRomanNumeralCommandExecuteTests
+    {
+        [TestMethod]
+        public void ShouldReturnBlankWhenZero()
+        {
+            var actual = new ConvertNumberToRomanNumeralCommand(0).Execute();
+            Assert.AreEqual("", actual);
+        }
+    }
+}
---
+++ C-Sharp-Roman-Numeral/RomanNumeral.Business/Converters/ConvertNumberToRomanNumeralCommand.cs
@@ -0,0 +1,16 @@
+﻿namespace RomanNumeral.Business.Converters
+{
+    public class ConvertNumberToRomanNumeralCommand
+    {
+        private readonly int _number;
+        public ConvertNumberToRomanNumeralCommand(int number)
+        {
+            _number = number;
+        }
+
+        public string Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
