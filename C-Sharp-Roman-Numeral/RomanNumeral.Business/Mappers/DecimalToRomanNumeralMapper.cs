b886ccf0718773352ee786c38e513da9d78898da
Chase Hardin <chase.hardin@Chases-MacBook-Pro-2.local>
2017-06-07 19:02:43 -0500

    Fix failing test - should convert 5.

--- C-Sharp-Roman-Numeral/RomanNumeral.Business/Mappers/DecimalToRomanNumeralMapper.cs
+++ C-Sharp-Roman-Numeral/RomanNumeral.Business/Mappers/DecimalToRomanNumeralMapper.cs
@@ -7,7 +7,8 @@ namespace RomanNumeral.Business.Mappers
         public static Dictionary<int, string> Mapper = new Dictionary<int, string>
         {
             { 1, "I" },
-            { 4, "IV" }
+            { 4, "IV" },
+            { 5, "V" }
         };
     }
 }
