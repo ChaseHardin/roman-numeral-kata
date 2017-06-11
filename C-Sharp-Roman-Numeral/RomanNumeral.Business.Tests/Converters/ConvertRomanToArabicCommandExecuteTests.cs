efd1b39daeee7221cee7ec18871ef7878802b42b
Chase Hardin <chase.hardin@Chases-MacBook-Pro-2.local>
2017-06-11 18:55:22 -0500

    add roman to arabic command with inital test.

---
+++ C-Sharp-Roman-Numeral/RomanNumeral.Business.Tests/Converters/ConvertRomanToArabicCommandExecuteTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanNumeral.Business.Converters;
+
+namespace RomanNumeral.Business.Tests.Converters
+{
+    [TestClass]
+    public class ConvertRomanToArabicCommandExecuteTests
+    {
+        [TestMethod]
+        public void ShouldConvertOne()
+        {
+            Assert.AreEqual(1, new ConvertRomanToArabicCommand("1").Execute());
+        }
+    }
+}
---
+++ C-Sharp-Roman-Numeral/RomanNumeral.Business/Converters/ConvertRomanToArabicCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace RomanNumeral.Business.Converters
+{
+    public class ConvertRomanToArabicCommand
+    {
+        private static string _romanNumeral;
+
+        public ConvertRomanToArabicCommand(string romanNumeral)
+        {
+            _romanNumeral = romanNumeral;
+        }
+
+        public int Execute()
+        {
+            return 1;
+        }
+    }
+}
