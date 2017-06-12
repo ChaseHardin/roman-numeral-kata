79745c1b8647f9fb4b6a083b3e00d90eb891d35e
Chase Hardin <chase.hardin@Chases-MacBook-Pro-2.local>
2017-06-11 19:56:52 -0500

    Fix failing test - should convert III.

--- C-Sharp-Roman-Numeral/RomanNumeral.Business/Converters/ConvertRomanToArabicCommand.cs
+++ C-Sharp-Roman-Numeral/RomanNumeral.Business/Converters/ConvertRomanToArabicCommand.cs
@@ -1,8 +1,11 @@
-﻿namespace RomanNumeral.Business.Converters
+﻿using System.Collections.Generic;
+
+namespace RomanNumeral.Business.Converters
 {
     public class ConvertRomanToArabicCommand
     {
         private static string _romanNumeral;
+        private int _number;
 
         public ConvertRomanToArabicCommand(string romanNumeral)
         {
@@ -11,7 +14,20 @@
 
         public int Execute()
         {
-            return _romanNumeral == "I" ? 1 : 2;
+            foreach (var t in _romanNumeral)
+            {
+                _number += RomanToArabicMapper.Mapper[t.ToString()];
+            }
+
+            return _number;
         }
+    }
+
+    public static class RomanToArabicMapper
+    {
+        public static Dictionary<string, int> Mapper = new Dictionary<string, int>
+        {
+            { "I", 1 }
+        }; 
     }
 }
